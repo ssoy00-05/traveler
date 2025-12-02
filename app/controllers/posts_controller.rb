773a0a6e7c1fdcbd371da129f30293e3f82b1ea8
ssoy00-05 <246840515+ssoy00-05@users.noreply.github.com>
2025-12-02 14:13:37 +0900

    [add]一覧ページから詳細ページへのリンク

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,7 +6,7 @@ class PostsController < ApplicationController
   def create
     post = Post.new(post_params)
     post.save
-    redirect_to '/top'
+    redirect_to post_path(post.id)
   end
  
   def index
@@ -16,7 +16,7 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
-  
+
   private
   def post_params
     params.require(:post).permit(:location, :text)
