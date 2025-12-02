35a857282d379d065fb7a3bc0a819600e99b87da
ssoy00-05 <246840515+ssoy00-05@users.noreply.github.com>
2025-12-02 14:05:14 +0900

    [add]投稿詳細ページ

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,6 +13,10 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
+  def show
+    @post = Post.find(params[:id])
+  end
+  
   private
   def post_params
     params.require(:post).permit(:location, :text)
